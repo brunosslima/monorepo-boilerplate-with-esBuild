c8e3059644f6e43cf8aae0d7792fbc5c8f8fcc70
Bruno <brunosslima@yahoo.com.br>
2025-02-11 16:23:38 -0300

    First commit made after developing BoilerPlate

---
+++ esbuild-config/client.prod.mjs
@@ -0,0 +1,20 @@
+import * as esbuild from 'esbuild';
+
+try {
+    await esbuild.build({
+        entryPoints: ['src/client/index.tsx'],
+        bundle: true,
+        sourcemap: false,
+        minify: true,
+        outfile: 'public/static/bundle.js',
+        define: {
+            'process.env.NODE_ENV': "'production'"
+        },
+        plugins: []
+    });
+
+    console.log('Client bundled successfully for production!');
+} catch (error) {
+    console.error('An error occurred during bundling:', error);
+    process.exit(1);
+}
